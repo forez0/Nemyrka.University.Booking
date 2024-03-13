251d82effbfb2dabb8be8bc7c9ae84051f294cd3
forez0 <nemurkadenus@gmail.com>
2024-03-13 17:11:38 +0200

    Commit

--- booking/manage.py
+++ booking/manage.py
@@ -3,10 +3,10 @@
 import os
 import sys
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web2.0.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'booking.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
