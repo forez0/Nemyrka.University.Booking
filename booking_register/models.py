a01dbc0684e38c753f55b29376a8241f63cb66b4
forez0 <115801262+forez0@users.noreply.github.com>
2024-03-14 00:14:15 +0200

    Update models.py

--- booking_register/models.py
+++ booking_register/models.py
@@ -1,31 +1,5 @@
-from django.contrib.auth.models import AbstractUser
+
 from django.db import models
 
-class User(AbstractUser):
-    email = models.CharField(max_length=250, unique=True, null=False, blank=False)
-    REGISTRATION_CHOICES = [
-        ('google', 'Google'),
-    ]
-    registration_method = models.CharField(
-        max_length=10,
-        choices=REGISTRATION_CHOICES,
-        default='google'
-    )
 
-    groups = models.ManyToManyField(
-        'auth.Group',
-        verbose_name='groups',
-        blank=True,
-        related_name='custom_user_set',
-        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
-    )
-    user_permissions = models.ManyToManyField(
-        'auth.Permission',
-        verbose_name='user permissions',
-        blank=True,
-        related_name='custom_user_set',
-        help_text='Specific permissions for this user.',
-    )
 
-    def __str__(self):
-        return self.username
