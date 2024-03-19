14a5b02786b70724392c782d53979504adcee564
forez0 <115801262+forez0@users.noreply.github.com>
2024-03-19 13:13:39 +0200

    Delete city_voting_registration directory

--- city_voting_registration/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CityVotingRegistrationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'city_voting_registration'
