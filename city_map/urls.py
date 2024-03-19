10d75ad91c6400d48ed1fd59b0a7d237455a5f2c
forez0 <115801262+forez0@users.noreply.github.com>
2024-03-19 13:13:31 +0200

    Delete city_map directory

--- city_map/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from city_map import views
-
-urlpatterns = [
-    path('city_map/', views.city_map, name='city_map'),
-]
--- city_map/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-
-# Create your views here.
-def city_map(request):
-    return render(request, 'city_map.html')
