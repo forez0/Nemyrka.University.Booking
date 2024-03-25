3054337933e6aa03c4540b9b8775296396bfbf3f
forez0 <nemurkadenus@gmail.com>
2024-03-25 22:50:58 +0200

    з

--- booking/urls.py
+++ booking/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from booking_register import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', views.register, name='register'),
